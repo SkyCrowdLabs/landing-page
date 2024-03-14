1796ac7c225fe19a2b8ac44c296cae1b46dfe851
Paul Ilaga <pgilaga@gmail.com>
2024-03-14 20:33:20 +0800

    Add more squigglies

--- src/app/sections/GetInTouch.tsx
+++ src/app/sections/GetInTouch.tsx
@@ -5,7 +5,21 @@ import Button from '../components/Button';
 
 const GetInTouch = () => {
   return (
-    <div className="bg-white bg-hero-pattern bg-cover px-6 py-24 sm:py-32 lg:px-8">
+    <div className="relative bg-white bg-hero-pattern bg-cover px-6 py-24 sm:py-32 lg:px-8">
+      <Image
+        src="/squiggly-7.svg"
+        alt="squiggly graphics"
+        height={320}
+        width={330}
+        className="absolute -left-12 -top-32"
+      />
+      <Image
+        src="/squiggly-1.svg"
+        alt="squiggly graphics"
+        height={320}
+        width={330}
+        className="absolute -right-12 top-20 -rotate-[45deg] scale-125"
+      />
       <div className="mx-auto max-w-4xl text-center">
         <SectionTitle title={'Start a project'} />
         <SectionHeader header={'Let’s Work Together!'} />
