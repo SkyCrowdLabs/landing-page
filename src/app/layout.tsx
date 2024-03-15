06f0890df31014ef7d7550dee3934d6fdc229b93
Paul Ilaga <pgilaga@gmail.com>
2024-03-15 17:54:53 +0800

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Lato } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const lato = Lato({
   subsets: ['latin'],
